0bc8085915cd8ed3944f351a5195f722d47b2910
sophiamarie89 <Sophie@dhcp-210-145.wireless.bowdoin.edu>
2015-12-05 16:05:17 -0500

    Now only the assignment of values is buggy (yay?)

--- flow.cc
+++ flow.cc
@@ -29,40 +29,38 @@ struct Traffic{
 	const static Doub p_max = 1.0;
 	const static Doub x_max = 1.0;
 
-	Doub delta;
 
 
 	Doub perturb;
 	Doub rho_bar;
 
+	int dim;
 
 	MatDoub rho_matrix;
 	VecDoub road;
-	Doub size_road;
-	Doub t;
+	Doub size_road = 12;
+	Doub t = 6;
+
+	Doub delta = 1.0/ (Doub)size_road;
 
 
-	Traffic(Doub perturb_in, Doub rho_bar_in, Doub size_road_in, Doub t_in) {
+
+	Traffic(Doub perturb_in, Doub rho_bar_in) {
 		perturb = perturb_in * p_max;
-		size_road = size_road_in;
 		rho_bar = rho_bar_in * p_max;
-		t = t_in;
-		delta = 1.0/ (size_road + 1.0);
-
-		//road.resize(size_road);
-		rho_matrix.assign(size_road, t, 0);
 	}
 
 	void init(){
+		rho_matrix.assign(t,size_road, 1.0);
 
 		//initalize values at t = 0
 		int index = 0;
-		for(Doub x= 0.0; x < x_max; x+= delta){
+		for(Doub x = 0.0; x < x_max; x += delta){
 			
 			Doub cur_rho = rho_bar + perturb
 				* exp(-( x - x_center)* ( x - x_center) / (lambda * lambda));
 
-			//road[index] = cur_rho;
+			road[index] = cur_rho;
 			rho_matrix[index][0] = cur_rho;
 
 			index++;
@@ -73,21 +71,21 @@ struct Traffic{
 
 	void print_matrix(){
 
-		for(int i = 0; i < size_road; i++){
+		for(int i = 0; i < t; i++){
 			
-			cout << "\n";
 
-			for(int j = 0; j < t; j++){
+			for(int j = 0; j < size_road; j++){
 
-				cout << setw(5) << rho_matrix[size_road][j];
+				cout << setw(12) << setprecision(3) << rho_matrix[i][j];
 
 			}
+			cout << "\n";
 		}
 	}
 
 	void print_road(){
 		for(int i = 0; i < size_road; i++){
-			cout << setw(5) << road[i];
+			cout << setw(5) << setprecision(3) << road[i];
 		}
 	}
 
@@ -97,16 +95,16 @@ struct Traffic{
 
 int main(){
 
-	Traffic traf(10e-3, 0.5, 3.0, 4.0);
+	Traffic traf(10e-3, 0.5);
 
-	traf.print_matrix();
-	traf.print_road();
+	//traf.print_matrix();
+	//traf.print_road();
 
 	traf.init();
 
 
 	traf.print_matrix();
-	traf.print_road();
+	//traf.print_road();
 
 	return 0;
 }
